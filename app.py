dfe8a55a8b21e04ecc53341cf6475bdcc2b8aef2
GrandeSasuke <158407213+GrandeSasuke@users.noreply.github.com>
2026-03-19 18:59:56 -0300

    Exibir titulo e coletar valores

---
+++ app.py
@@ -0,0 +1,13 @@
+import funcoes
+import os
+
+
+def main():
+	"""Inicia e mantém o ciclo de vida da Calculadora do Programador"""
+	os.system('cls')
+	funcoes.exibir_titulo()
+	funcoes.coletar_valores()
+
+if __name__ == '__main__':
+	main()
+	
---
+++ funcoes.py
@@ -0,0 +1,14 @@
+import os
+
+
+def exibir_titulo():
+	print('ℂ𝕒𝕝𝕔𝕦𝕝𝕒𝕕𝕠𝕣𝕒 𝕕𝕠 ℙ𝕣𝕠𝕘𝕣𝕒𝕞𝕒𝕕𝕠𝕣\n')
+
+
+def coletar_valores():
+	print('=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~')
+	base = int(input('Digite a base do número para calculo: '))
+	valor = int(input('Digite o valor: '))
+	print('=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~')
+	return base and valor
+
